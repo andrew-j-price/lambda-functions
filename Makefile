25e5a1bbb3f53fc8218ddd08d129f483075c9481
Andrew Price <andrew.j.price@gmail.com>
2021-12-30 16:23:58 -0500

    testing for health_check_py

--- Makefile
+++ Makefile
@@ -28,6 +28,29 @@ exec_py:
 	docker-compose exec pyfunctions bash
 
 
+# function:health_check_py
+function_health_check_py: run_health_check_py line_breaks1 test_health_check_py line_breaks2 black_check_health_check_py line_breaks3 flake8_health_check_py
+
+run_health_check_py:
+	docker-compose exec -T pyfunctions python /git/health_check_py/lambda_function.py
+
+test_health_check_py:
+	docker-compose exec -T pyfunctions pytest --verbose /git/health_check_py/test_lambda_function.py
+
+black_apply_health_check_py:
+	docker-compose exec -T pyfunctions black --line-length 120 /git/health_check_py/lambda_function.py && \
+	docker-compose exec -T pyfunctions black --line-length 120 /git/health_check_py/test_lambda_function.py
+
+black_check_health_check_py:
+	docker-compose exec -T pyfunctions black --check --line-length 120 /git/health_check_py/lambda_function.py && \
+	docker-compose exec -T pyfunctions black --check --line-length 120 /git/health_check_py/test_lambda_function.py
+
+flake8_health_check_py:
+	docker-compose exec -T pyfunctions flake8 --max-line-length 120 /git/health_check_py/lambda_function.py && \
+	docker-compose exec -T pyfunctions flake8 --max-line-length 120 /git/health_check_py/test_lambda_function.py
+
+
+
 # function:pass_fail_py
 function_pass_fail_py: run_pass_fail_py line_breaks1 test_pass_fail_py line_breaks2 black_check_pass_fail_py line_breaks3 flake8_pass_fail_py
 
---
+++ health_check_py/test_lambda_function.py
@@ -0,0 +1,97 @@
+import os
+import sys
+import unittest
+from unittest.mock import MagicMock, patch
+from lambda_function import lambda_handler, LambdaHandler
+
+
+@patch.dict(os.environ, {"AWS_REGION": "unit-test-1"})
+class TestLambdaHandlerClass(unittest.TestCase):
+    def test_class_init(self):
+        lh = LambdaHandler()
+        assert lh.return_code == 0
+
+    @patch("requests.get")
+    def test_get_attest_success(self, mock_req):
+        # requests response
+        mock_obj = MagicMock()
+        mock_obj.status_code = 200
+        mock_obj.text = '{"host_name": "abc123", "uri": "/default"}'
+        mock_req.return_value = mock_obj
+        response = LambdaHandler().get_attest()
+        mock_req.assert_called_once()
+        assert response == "abc123"
+
+    @patch("requests.get")
+    def test_get_attest_failure(self, mock_req):
+        mock_req.status_code = 404
+        response = LambdaHandler().get_attest()
+        mock_req.assert_called_once()
+        assert response is None
+
+    def test_get_force_failure_false(self):
+        event = {"something": "else"}
+        response = LambdaHandler().get_force_failure(event)
+        assert response is False
+
+    def test_get_force_failure_true(self):
+        event = {"force_failure": True}
+        lh = LambdaHandler()
+        response = lh.get_force_failure(event)
+        assert lh.return_code == 13
+        assert response is True
+
+    @patch("lambda_function.LambdaHandler.get_ipv6")
+    @patch("lambda_function.LambdaHandler.get_ipv4")
+    @patch("lambda_function.LambdaHandler.get_force_failure")
+    @patch("lambda_function.LambdaHandler.get_attest")
+    def test_actions(self, mock_attest, mock_force, mock_ipv4, mock_ipv6):
+        mock_attest.return_value = "abc123"
+        mock_force.return_value = False
+        mock_ipv4.return_value = None
+        mock_ipv6.return_value = None
+        event = {"unit": "test"}
+        response = LambdaHandler().actions(event)
+        assert isinstance(response, dict)
+        assert response.get("region") == "unit-test-1"
+        assert response.get("return_code") == 0
+        assert response == {
+            "attest_container": "abc123",
+            "force_failure": False,
+            "ipv4": None,
+            "ipv6": None,
+            "region": "unit-test-1",
+            "return_code": 0,
+        }
+
+    @patch("lambda_function.LambdaHandler.actions")
+    def test_main(self, mock_actions):
+        mock_actions.return_value = {
+            "attest_container": "abc123",
+            "force_failure": False,
+            "ipv4": None,
+            "ipv6": None,
+            "region": "unit-test-1",
+            "return_code": 0,
+        }
+        response = LambdaHandler().main()
+        assert isinstance(response, dict)
+        assert response.get("region") == "unit-test-1"
+        assert response.get("return_code") == 0
+        assert response == {
+            "attest_container": "abc123",
+            "force_failure": False,
+            "ipv4": None,
+            "ipv6": None,
+            "region": "unit-test-1",
+            "return_code": 0,
+        }
+
+    @patch("lambda_function.LambdaHandler")
+    def test_lambda_handler_entry(self, mock_class):
+        lambda_handler(event=None, context=None)
+        assert mock_class.called_once_with()
+
+    def test_python_version(self):
+        assert sys.version_info[0] == 3
+        assert sys.version_info[1] == 8
